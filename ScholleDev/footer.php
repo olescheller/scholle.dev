e4a110ebf0abda3d468df7625b74ba5db72fae0d
Ole Scheller <olescheller87@gmail.com>
2020-11-19 23:36:27 +0100

    Update footer

--- ScholleDev/footer.php
+++ ScholleDev/footer.php
@@ -7,22 +7,21 @@
 
             <div>
                 <div class="footer-caption">Contact</div>
-                <div>Twitter</div>
-                <div>LinkedIn</div>
-                <div>XING</div>
-                <div>GitHub</div>
+                <div><a target="_blank" href="https://twitter.com/ole_scheller">Twitter</a></div>
+                <div><a target="_blank" href="https://www.linkedin.com/in/ole-scheller-885428200/">LinkedIn</a></div>
+                <div><a target="_blank" href="https://www.xing.com/profile/Ole_Scheller3/cv">XING</a></div>
             </div>
 
 
             <div>
                 <div class="footer-caption">scholle.dev</div>
-                <div>Impressum / Imprint</div>
+                <div><a href="<?php echo get_home_url() . "/imprint" ?>">Impressum / Imprint</a></div>
             </div>
 
 
             <div>
                 <div class="footer-caption">Further web presences</div>
-                <div>scholle.me</div>
+                <div><a target="_blank" href="https://www.scholle.me">scholle.me</a></div>
             </div>
 
 
